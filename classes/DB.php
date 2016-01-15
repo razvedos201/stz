5ffbb40e6b32e17560d02e87670aa216ad84da16
razvedos201 <razvedos201@yandex.ru>
2016-01-15 21:43:58 +0300

    Всё заебись

--- classes/DB.php
+++ classes/DB.php
@@ -8,10 +8,9 @@ class DB
 
         try {
             $settings = parse_ini_file(__DIR__.'/../db_settings.ini');
-            $dsn = $settings[database][driver].':dbname='.$settings[database][schema].
-                    ';host='.$settings[database][host];
-            $username = $settings[database][username];
-            $password = $settings[database][password];
+            $dsn =$settings['driver'].':dbname='.$settings['schema'].';host='.$settings['host'];
+            $username = $settings['username'];
+            $password = $settings['password'];
             $dbh = new PDO($dsn,$username,$password);
         }catch (PDOException $e){
             echo 'Подключение не удалось: ' . $e->getMessage();
