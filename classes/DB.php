417cb35bc5e49743c82f906df67c2624349092c4
razvedos201 <razvedos201@yandex.ru>
2016-01-15 22:29:39 +0300

    Торртик несу блять

--- classes/DB.php
+++ classes/DB.php
@@ -2,21 +2,26 @@
 
 class DB
 {
-
-    public function query($sql,$arr,$class = 'stdClass')
+    private $dbh;
+    public function __construct()
     {
-
         try {
             $settings = parse_ini_file(__DIR__.'/../db_settings.ini');
             $dsn =$settings['driver'].':dbname='.$settings['schema'].';host='.$settings['host'];
             $username = $settings['username'];
             $password = $settings['password'];
-            $dbh = new PDO($dsn,$username,$password);
+            $this->dbh = new PDO($dsn,$username,$password);
         }catch (PDOException $e){
             echo 'Подключение не удалось: ' . $e->getMessage();
             die;
         }
-            $sth = $dbh->prepare($sql);
+    }
+
+    public function query($sql,$arr,$class = 'stdClass')
+    {
+
+
+            $sth = $this->dbh->prepare($sql);
 
             $sth->execute($arr);
 
