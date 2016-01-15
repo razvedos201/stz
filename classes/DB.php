dac66fc2453fd40a92ea18eef642bccfe2a8db11
razvedos201 <razvedos201@yandex.ru>
2016-01-15 21:13:50 +0300

    Настройки хранятся в конфиге

--- classes/DB.php
+++ classes/DB.php
@@ -7,7 +7,7 @@ class DB
     {
 
         try {
-            $settings = [];
+
             $settings = parse_ini_file(__DIR__.'/../db_settings.ini');
             $dsn = $settings[database][driver].':dbname='.$settings[database][schema].
                     ';host='.$settings[database][host];
